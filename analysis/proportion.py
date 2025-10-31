6f33fbc681a154cd5afe04f782655316956347fe
MoyukhShabon <moyukh.work@gmail.com>
2025-10-31 20:20:32 +0800

    updated proportion calculation script

--- analysis/proportion.py
+++ analysis/proportion.py
@@ -1,6 +1,18 @@
+#!/usr/bin/env python
 import polars as pl
 import os
 import glob
+from lxml import etree
+
+def get_test_type(xml_path: int) -> str:
+	
+	if not os.path.exists(xml_path):
+		raise FileNotFoundError(f"No file found at {xml_path}")
+	
+	tree = etree.parse(xml_path)
+	root = tree.getroot()
+	return root.xpath("//TestType")[0].text
+
 
 def calc_arti_prop(artifact_pred_paths: list) -> pl.DataFrame:
 	arti_prop = []
@@ -10,10 +22,15 @@ def calc_arti_prop(artifact_pred_paths: list) -> pl.DataFrame:
 		artifact_pred = pl.read_csv(path, separator="\t")
 
 		snvs = artifact_pred.shape[0]
-		artifacts = artifact_pred.filter(pl.col("pred") == False).shape[0]
+		artifacts = artifact_pred.filter(~pl.col("pred")).shape[0]
+
+		sample_name = os.path.basename(path).split(".")[0]
+		xml_path = f"../data/{sample_name}.xml"
+		test_type = get_test_type(xml_path)
 
 		sample_stats = {
-			"sample_name" : os.path.basename(path).split(".")[0],
+			"sample_name" : sample_name,
+			"test_type": test_type,
 			"n_snv" : snvs,
 			"n_real" : (snvs - artifacts),
 			"n_artifacts" : artifacts,
@@ -25,101 +42,86 @@ def calc_arti_prop(artifact_pred_paths: list) -> pl.DataFrame:
 	arti_prop = pl.DataFrame(arti_prop).sort("proportion", descending=True)
 	return arti_prop
 
-def get_stats(arti_prop: pl.DataFrame) -> None:
+def get_stats(arti_prop: pl.DataFrame, damage_type: str = None) -> None:
+	
+	prop_test_type = (
+		arti_prop
+		.group_by("test_type")
+		.agg(
+			pl.col("proportion").mean().alias("mean_proportion"),
+			pl.col("proportion").count().alias("count"),
+			pl.col("proportion").max().alias("max_proportion")
+		)
+	)
+
+	prop_test_type_with_artifact = (
+		arti_prop
+		.filter(pl.col("proportion") > 0)
+		.group_by("test_type")
+		.agg(
+			pl.col("proportion").mean().alias("mean_proportion"),
+			pl.col("proportion").count().alias("count_>=1_artifact")
+		)
+	)
+	
 	mean_pct = arti_prop["proportion"].mean() * 100
 	max_pct = arti_prop["proportion"].max() * 100
 	n_samples = arti_prop.shape[0]
-	n_with_artifacts = arti_prop.filter(pl.col("proportion") > 0).shape[0]
+	has_artifact_df = arti_prop.filter(pl.col("proportion") > 0)
+	n_with_artifacts = has_artifact_df.shape[0]
+	mean_pct_arti_samples = has_artifact_df["proportion"].mean() * 100
+
+	if damage_type:
+		print("Damage Type:", damage_type.upper())
 
 	print(f"Samples analyzed: {n_samples}")
 	print(f"Samples with ≥1 predicted artifact: {n_with_artifacts} ({n_with_artifacts / n_samples * 100:.1f}%)")
 	print(f"Mean proportion of artifactual SNVs: {mean_pct:.2f}%")
 	print(f"Max proportion of artifactual SNVs: {max_pct:.2f}%")
-	print("")
+	print(f"Mean proportion of artifactual SNVs within samples with ≥1 predicted artifact: {mean_pct_arti_samples:.2f}%")
+ 
+	print("Results stratified by test type:")
+	for i in range(prop_test_type.shape[0]):
+
+		test_type = prop_test_type_with_artifact[i, "test_type"]
+
+		print(f"\t{test_type} samples: {prop_test_type[i, "count"]}")
+		print(f"\t{test_type} max artifact proportion within samples: {prop_test_type[i, "max_proportion"] * 100:.2f}%")
+		print(f"\t{test_type} mean artifact proportion within samples: {prop_test_type[i, "mean_proportion"] * 100:.2f}%")
+
+		prop_test_type_with_artifact_filtered = prop_test_type_with_artifact.filter(pl.col("test_type") == test_type)
+
+		if prop_test_type_with_artifact_filtered.shape[0]:
+			print(f"\t{test_type} samples with >1 artifact: {prop_test_type_with_artifact_filtered[0, "count_>=1_artifact"]}")
+			print(f"\t{test_type} mean artifact proportion within samples with ≥1 detected artifacts: {prop_test_type_with_artifact_filtered[0, "mean_proportion"] * 100:.2f}%")
+
+		## Create spacing
+		print()
+	print()
 
 def get_res(damage_type: str, fp_cut: float) -> pl.DataFrame:
 	
-	search_patern = f"../{damage_type.lower()}-snvf/*/*.pred_fp-cut_{fp_cut:.0e}.tsv"
-	arti_pred_paths = glob.glob(search_patern)
+	search_pattern = f"../{damage_type.lower()}-snvf/*/*.pred_fp-cut_{fp_cut:.0e}.tsv"
+	arti_pred_paths = glob.glob(search_pattern)
  
 	if not arti_pred_paths:
-		raise FileNotFoundError (f"No predictions made with fp-cut: {fp_cut:.0e} at {search_patern}")
+		raise FileNotFoundError (f"No predictions made with fp-cut: {fp_cut:.0e} at {search_pattern}")
 
 	proportions = calc_arti_prop(arti_pred_paths)
 
-	print(f"fp-cut: {fp_cut}")
-	get_stats(proportions)
+	print(f"FP-cut: {fp_cut}")
+	get_stats(proportions, damage_type=damage_type)
 
 	proportions.write_csv(f"{damage_type}_proportions_per_sample.fp-cut_{fp_cut:.0e}.tsv", separator="\t")
 	return proportions
 
-# %% [markdown]
 # ## FFPE
-
 get_res("ffpe", 1e-08)
 
 get_res("ffpe", 5e-01)
 
-# %% [markdown]
-# #### Observations - FFPE
-# 
-# **2025/10/24**
-# 
-# Out of 204 samples analyzed so far.
-# 
-# - Using a stringent fp-cut of 1e-08: 
-# 	- 39/204 samples (19.1%) was observed to possess artifacts. 
-# 	- In terms of proportion of artifactual SNVs, the max was 25%, mean was 0.94 %
-# 
-# - Using a relaxed fp-cut of 5e-01: 
-# 	- 9/204 samples (4.4%) was observed to possess artifacts. 
-# 	- In terms of proportion of artifactual SNVs, the max was 25%, mean was 0.24 %
-# 
-# 
-# **2025/10/27**
-# 
-# Out of 425 samples analyzed so far.
-# 
-# - Using a stringent fp-cut of 1e-08: 
-# 	- 81/425 samples (19.1%) was observed to possess artifacts. 
-# 	- In terms of proportion of artifactual SNVs, the max was 25%, mean was 0.93 %
-# 
-# - Using a relaxed fp-cut of 5e-01: 
-# 	- 24/425 samples (5.6%) was observed to possess artifacts. 
-# 	- In terms of proportion of artifactual SNVs, the max was 25%, mean was 0.26 %
 
-# %% [markdown]
 # ## OxoG
-
 get_res("oxog", 1e-08)
 
 get_res("oxog", 5e-01)
-
-# %% [markdown]
-# #### Observations - OxoG
-# 
-# **2025/10/24**
-# 
-# Out of 204 samples analyzed so far.
-# 
-# - Using a stringent fp-cut of 1e-08: 
-# 	- 15/204 samples (7.3%) was observed to possess artifacts. 
-# 	- In terms of proportion of artifactual SNVs within a sample, the max was 25%, mean was 0.37%
-# 
-# - Using a relaxed fp-cut of 5e-01: 
-# 	- 5/204 samples (2.45%) was observed to possess artifacts. 
-# 	- In terms of proportion of artifactual SNVs within a sample, the max was 25%, mean was 0.20%
-# 
-# **2025/10/27**
-# 
-# Out of 425 samples analyzed so far.
-# 
-# - Using a stringent fp-cut of 1e-08: 
-# 	- 36/425 samples (8.5%) was observed to possess artifacts. 
-# 	- In terms of proportion of artifactual SNVs within a sample, the max was 25%; mean was 0.34%
-# 
-# - Using a relaxed fp-cut of 5e-01: 
-# 	- 13/425 samples (3.1%) was observed to possess artifacts. 
-# 	- In terms of proportion of artifactual SNVs within a sample, the max was 25%; mean was 0.16%
-
-
