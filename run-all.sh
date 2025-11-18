c4515be54e88d8e5dc3fd2fdce67a2f2816d5e49
MoyukhShabon <moyukh.work@gmail.com>
2025-11-18 17:38:31 +0800

    added script to run entire pipeline

---
+++ run-all.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# set -euo pipefail
+
+echo -e "linking Foundation medicine data to data/ directory \n"
+cd data
+bash link.sh
+cd -
+
+echo -e "\nPreparing VCF SNVF scripts \n"
+python prepare-vcf.py
+
+echo -e "\nRunning VCF SNVF\n"
+cd vcf-ffpe-snvf/scripts
+dlazy .
+cd -
+
+cd vcf-oxog-snvf/scripts
+dlazy .
+cd -
+
+echo -e "\nPreparing XML SNV Tables and making XML SNVF results by subsetting of VCF SNVF results \n"
+python prepare-xml.py
+
+echo -e "\nCreating VUS excluded set from FFPE SNVF results  \n"
+python filter_vus.py
+
+
+echo -e "\nPredicting Artifacts \n"
+Rscript predict.R
+Rscript predict.R --fp.cut 1e-08
+
+echo -e "\nCalculating Proportion of artifacts per sample \n"
+cd analysis
+python proportion.py | tee summary_statistics.txt
+
+echo -e "\ncoCollecting all artifacts into a table \n"
+python collect-artifacts.py
+
