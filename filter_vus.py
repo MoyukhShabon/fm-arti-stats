0c8524a163c057b850f8c24756f9fb7fff35b658
MoyukhShabon <moyukh.work@gmail.com>
2026-01-16 02:46:49 +0800

    updated vus filtering script

--- filter_vus.py
+++ filter_vus.py
@@ -42,18 +42,19 @@ def get_variant_properties(xml_path: str) -> pl.DataFrame:
 		
 	return pl.DataFrame(variant_properties)
 
-def annotate_mobsnvf_res(mobsnvf_res_path: str, annotated_snvs: pl.DataFrame) -> pl.DataFrame:
-	mobsnvf_raw = pl.read_csv(mobsnvf_res_path, separator = "\t")
-	if len(mobsnvf_raw.columns) > 5:
-		return mobsnvf_raw
+def annotate_res(mobsnvf_res_path: str, annotated_snvs_path: str) -> pl.DataFrame:
+	raw_res = pl.read_csv(mobsnvf_res_path, separator = "\t")
+	if "is_vus" in raw_res.columns:
+		return raw_res
 	else:
-		return mobsnvf_raw.join(annotated_snvs, on = ["chrom", "pos", "ref", "alt"], how = "left")
+		annotated_snvs = pl.read_csv(annotated_snvs_path, separator="\t")
+		return raw_res.join(annotated_snvs, on = ["chrom", "pos", "ref", "alt"], how = "left")
 
 # %% [markdown]
 # ### VUS exclusion
 
 no_bam_list = pl.read_csv("annot/vcf-no_bam.tsv", separator="\t")
-xml_snv_annot_paths = sorted(glob.glob('xml-variants/*/*.tsv'))
+xml_snv_annot_paths = sorted(glob.glob('xml-variants/*/*.with-indels.tsv'))
 
 
 no_variants = []
@@ -65,16 +66,16 @@ for i, path in enumerate(xml_snv_annot_paths):
 		print(f"{i+1}. Sample {sample_name} has no associated BAM. skipping...")
 		continue
 	
-	print(f"{i+1}. Excluding VUS from mobsnvf results of {sample_name}")
+	print(f"{i+1}. Excluding VUS from mobsnvf and microsec results of {sample_name}")
 	
 	xml_path = return_if_exists(f"data/{sample_name}.xml")
 	ffpe_snvf_path = return_if_exists(f"xml-ffpe-snvf/{sample_name}/{sample_name}.mobsnvf.ffpe.snv")
 	oxog_snvf_path = return_if_exists(f"xml-oxog-snvf/{sample_name}/{sample_name}.mobsnvf.oxog.snv")
-	micr_svf_path = return_if_exists(f"xml-micr-svf/{sample_name}/{sample_name}.microsec.tsv") ## May exclude samples with only indels
+	micr_svf_path = return_if_exists(f"xml-micr-svf/{sample_name}/{sample_name}.microsec.tsv")
 	
-	annotated_snvs = pl.read_csv(path, separator="\t")
-	mobsnvf_ffpe_annot = annotate_mobsnvf_res(ffpe_snvf_path, annotated_snvs)
-	mobsnvf_oxog_annot = annotate_mobsnvf_res(ffpe_snvf_path, annotated_snvs)
+	mobsnvf_ffpe_annot = annotate_res(ffpe_snvf_path, path)
+	mobsnvf_oxog_annot = annotate_res(ffpe_snvf_path, path)
+	msec_micr_annot = annotate_res(ffpe_snvf_path, path)
 	
 	if ("is_vus" not in mobsnvf_ffpe_annot.columns):
 		# XMLs seem to only have variant properties with VUS being true
@@ -84,17 +85,13 @@ for i, path in enumerate(xml_snv_annot_paths):
 		# Subset variants
 		mobsnvf_ffpe_no_vus = mobsnvf_ffpe_annot.join(var_properties_vus, left_on="protein_effect", right_on="variant_name", how="anti")
 		mobsnvf_oxog_no_vus = mobsnvf_oxog_annot.join(var_properties_vus, left_on="protein_effect", right_on="variant_name", how="anti")
+		msec_micr_no_vus = msec_micr_annot.join(var_properties_vus, left_on="protein_effect", right_on="variant_name", how="anti")
 
 	else:
 		mobsnvf_ffpe_no_vus = mobsnvf_ffpe_annot.filter(~pl.col("is_vus"))
 		mobsnvf_oxog_no_vus = mobsnvf_oxog_annot.filter(~pl.col("is_vus"))
-	
-	
-	keep_annotations = True
-	
-	if not keep_annotations:
-		mobsnvf_ffpe_no_vus = mobsnvf_ffpe_no_vus.select("chrom", "pos", "ref", "alt", "FOBP")
-		mobsnvf_oxog_no_vus = mobsnvf_oxog_no_vus.select("chrom", "pos", "ref", "alt", "FOBP")
+		msec_micr_no_vus = msec_micr_annot.filter(~pl.col("is_vus"))
+
 	
 	if mobsnvf_ffpe_no_vus.is_empty():
 		print(f"\t{sample_name} has no variants after VUS exclusion")
@@ -103,6 +100,7 @@ for i, path in enumerate(xml_snv_annot_paths):
 	
 	mobsnvf_ffpe_no_vus.write_csv(ffpe_snvf_path.replace(".snv", ".no_vus.snv"), separator="\t")
 	mobsnvf_oxog_no_vus.write_csv(oxog_snvf_path.replace(".snv", ".no_vus.snv"), separator="\t")
+	msec_micr_no_vus.write_csv(micr_svf_path.replace(".tsv", ".no_vus.tsv"), separator="\t")
 
    
 pl.DataFrame(no_variants).write_csv("annot/xml-no_snv-vus_filtration.txt", separator="\t", include_header=False)
